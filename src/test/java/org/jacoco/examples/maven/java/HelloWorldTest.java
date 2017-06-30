77a86968786b45b6fa171514a92a1e2ee6339a8e
Yuriy Yunikov <yyunikov@Yuriys-MacBook-Pro.local>
2017-06-30 17:54:49 +0300

    Removed test

--- src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
@@ -1,9 +1,6 @@
 package org.jacoco.examples.maven.java;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
-import org.junit.Test;
 
 public class HelloWorldTest {
 
@@ -13,10 +10,4 @@ public class HelloWorldTest {
 	public void setup() {
 		subject = new HelloWorld();
 	}
-	
-	@Test
-	public void testGetMessage() {
-		assertEquals("Hello World!", subject.getMessage(false));
-	}
-	
 }
