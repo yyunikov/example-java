9486f313f4298d31111f2b9f98ef2177bd370259
yyunikov <yyunikov@ws211.n-ix.local>
2017-07-11 15:44:21 +0300

    Created new file with tests

---
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorld2Test.java
@@ -0,0 +1,22 @@
+package org.jacoco.examples.maven.java;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorld2Test {
+
+	private HelloWorld subject;
+
+	@Before
+	public void setup() {
+		subject = new HelloWorld();
+	}
+
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello World!", subject.getMessage(false));
+		assertEquals("Hello Universe!", subject.getMessage(true));
+	}
+}
